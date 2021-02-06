67bd5abea8a1028956784a1973b69ffaf18905f7
Shahinul Hoque <onyhoque@gmail.com>
2021-02-06 20:19:07 +0600

    02/06/2021:20:19:07.08

--- git push.bat
+++ git push.bat
@@ -1,5 +1,9 @@
+@echo off
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+set mytime=%time%
+@echo on
 git add .
-git commit -m "first commit"
+git commit -m "%mydate%:%mytime%"
 git branch -M main
 git remote add origin https://github.com/OnyHoque/onyhoque.github.io.git
 git push -u origin main
